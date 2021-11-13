6d51fce9954d3e91aa8a2c46b67234491be18484
Asha Krishna S <68347359+AshaKrishnaS@users.noreply.github.com>
2021-11-13 21:43:33 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace forloop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            for (int i = 1; i <= 5; i++)
+            {
+                Console.WriteLine("Hello World!");
+            }
+        }
+    }
+}
